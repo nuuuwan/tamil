fab94c760be63a098b848a1f9fb6a93c403cfac4
nuuuwan <nuuuwan@gmail.com>
2026-01-19 11:44:21 +0530

    Refactor MetadataView to include loading skeleton and improve image loading handling

--- src/view/moles/MetadataView.js
+++ src/view/moles/MetadataView.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Box, Typography, Skeleton } from "@mui/material";
 import LoadingView from "../atoms/LoadingView";
 
 export default function MetadataView({ metadata }) {
-  if (!metadata) {
-    return <LoadingView />;
-  }
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    setImageLoaded(false);
+  }, [metadata]);
+
+  const showSkeleton =
+    !metadata || (metadata && metadata.picturePath && !imageLoaded);
 
   return (
     <Box
@@ -15,7 +20,20 @@ export default function MetadataView({ metadata }) {
         textAlign: "center",
       }}
     >
-      {metadata.picturePath && (
+      {showSkeleton && (
+        <Box sx={{ marginBottom: "20px" }}>
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height={400}
+            sx={{ marginBottom: "20px" }}
+          />
+        </Box>
+      )}
+
+      {!metadata && <LoadingView />}
+
+      {metadata && metadata.picturePath && (
         <Box sx={{ marginBottom: "20px" }}>
           <img
             src={metadata.picturePath}
@@ -26,23 +44,30 @@ export default function MetadataView({ metadata }) {
               top: 0,
               left: 0,
               right: 0,
+              display: imageLoaded ? "block" : "none",
             }}
+            onLoad={() => setImageLoaded(true)}
           />
         </Box>
       )}
-      <Box sx={{ marginBottom: "20px" }}>
-        <Typography variant="h3" fontWeight="bold" align="center">
-          {metadata.taWord}
-        </Typography>
-      </Box>
 
-      {metadata.soundPath && (
-        <Box sx={{ marginBottom: "20px" }}>
-          <audio controls>
-            <source src={metadata.soundPath} type="audio/mpeg" />
-            Your browser does not support the audio element.
-          </audio>
-        </Box>
+      {metadata && (
+        <>
+          <Box sx={{ marginBottom: "20px" }}>
+            <Typography variant="h3" fontWeight="bold" align="center">
+              {metadata.taWord}
+            </Typography>
+          </Box>
+
+          {metadata.soundPath && (
+            <Box sx={{ marginBottom: "20px" }}>
+              <audio controls>
+                <source src={metadata.soundPath} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            </Box>
+          )}
+        </>
       )}
     </Box>
   );
