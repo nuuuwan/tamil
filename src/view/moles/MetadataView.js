92e1bc8b53739bb4007832b746a4259c8fdfb04f
nuuuwan <nuuuwan@gmail.com>
2026-01-19 14:45:06 +0530

    Refactor MetadataView component to simplify image loading logic and enhance code clarity

--- src/view/moles/MetadataView.js
+++ src/view/moles/MetadataView.js
@@ -1,71 +1,52 @@
-import React, { useState, useEffect } from "react";
 import { Box, Typography, Skeleton } from "@mui/material";
-import LoadingView from "../atoms/LoadingView";
 import PlayAudioButton from "../atoms/PlayAudioButton";
+import LoadingView from "../atoms/LoadingView";
 
 export default function MetadataView({ metadata }) {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  useEffect(() => {
-    setImageLoaded(false);
-  }, [metadata]);
-
-  console.debug(metadata?.picturePath);
-
+  if (!metadata) {
+    return <LoadingView />;
+  }
   return (
     <Box
+      key={metadata.taWord}
       sx={{
         maxWidth: "600px",
         margin: "0 auto",
         textAlign: "center",
       }}
     >
-      {metadata && metadata.picturePath && !imageLoaded && (
+      <Box sx={{ marginBottom: "20px" }}>
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height={400}
+          sx={{ marginBottom: "20px" }}
+        />
+      </Box>
+      <Box sx={{ marginBottom: "20px" }}>
+        <img
+          src={metadata.picturePath}
+          alt={metadata.taWord}
+          style={{
+            position: "absolute",
+            width: "calc(min(100vw, 640px))",
+            height: "auto",
+            top: 0,
+            left: 0,
+            right: 0,
+          }}
+        />
+      </Box>
+      <Box sx={{ position: "absolute", bottom: 50, left: 0, right: 0 }}>
         <Box sx={{ marginBottom: "20px" }}>
-          <Skeleton
-            variant="rectangular"
-            width="100%"
-            height={400}
-            sx={{ marginBottom: "20px" }}
-          />
+          <Typography variant="h3" fontWeight="bold" align="center">
+            {metadata.taWord}
+          </Typography>
         </Box>
-      )}
-
-      {metadata && metadata.picturePath && (
-        <Box key={"image" + metadata.taWord} sx={{ marginBottom: "20px" }}>
-          <img
-            src={metadata.picturePath}
-            alt={metadata.enWord}
-            style={{
-              width: "calc(min(100vw, 640px))",
-              height: "auto",
-              top: 0,
-              left: 0,
-              right: 0,
-              display: imageLoaded ? "block" : "none",
-            }}
-            onLoad={() => setImageLoaded(true)}
-          />
+        <Box sx={{ marginBottom: "20px" }}>
+          <PlayAudioButton soundPath={metadata.soundPath} />
         </Box>
-      )}
-
-      {metadata && metadata.picturePath && imageLoaded && (
-        <Box key={"text" + metadata.taWord}>
-          <Box sx={{ marginBottom: "20px" }}>
-            <Typography variant="h3" fontWeight="bold" align="center">
-              {metadata.taWord}
-            </Typography>
-          </Box>
-
-          {metadata.soundPath && (
-            <Box sx={{ marginBottom: "20px" }}>
-              <PlayAudioButton soundPath={metadata.soundPath} />
-            </Box>
-          )}
-        </Box>
-      )}
-
-      {!imageLoaded && <LoadingView />}
+      </Box>
     </Box>
   );
 }
